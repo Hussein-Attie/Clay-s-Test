baf6ac53ae5954392b9689fc4056a7560441525e
Hussein Attie <Hussein10attie@gmail.com>
2021-02-06 01:28:39 +0200

    Heres the test project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float jumpforce;
+   
+    public delegate void PlayersAction();
+    public static event PlayersAction OnObstacleOvioded;
+    public static event PlayersAction Ondeath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+      
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0);
+            rb.AddForce(Vector3.up * jumpforce);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb.velocity.x < 0.3)
+        {
+            rb.velocity = new Vector2(3, rb.velocity.y);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+      
+        if(collision.collider.CompareTag("obstacle"))
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            if(Ondeath!= null)
+            {
+                Ondeath();
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("point"))
+        {
+            if(OnObstacleOvioded!= null)
+            {
+                OnObstacleOvioded();
+            }
+         
+        }
+        if (collision.CompareTag("Finish"))
+        {
+            if (OnObstacleOvioded != null)
+            {
+                OnObstacleOvioded();
+            }
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/UIupdates.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIupdates : MonoBehaviour
+{
+    int points;
+    int deaths;
+    public TextMeshProUGUI pointstext;
+    public TextMeshProUGUI deathtxt;
+    private static UIupdates instance;
+    ////void Awake()
+    ////{
+    ////    DontDestroyOnLoad(this);
+
+    ////    if (instance == null)
+    ////    {
+    ////        instance = this;
+    ////    }
+    ////    else
+    ////    {
+    ////        DestroyObject(gameObject);
+    ////    }
+    ////}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deaths = PlayerPrefs.GetInt("deaths");
+        points = PlayerPrefs.GetInt("points");
+        PlayerController.OnObstacleOvioded += Win ;
+        PlayerController.Ondeath           += Lose;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        Debug.Log(deaths);
+        pointstext.text = "Points :"+ PlayerPrefs.GetInt("points").ToString();
+        deathtxt.text = "Deaths :" + PlayerPrefs.GetInt("deaths").ToString();
+    }
+    void Win()
+    {
+        points++;
+        PlayerPrefs.SetInt("points", points);
+    }
+    void Lose()
+    {
+        deaths++;
+        PlayerPrefs.SetInt("deaths", deaths);
+    }
+   
+}
